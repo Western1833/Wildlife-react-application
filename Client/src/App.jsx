6e5005eaa2ce388ac159101a15f4f836cefbaf6e
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-04-28 01:58:24 +0300

    add login page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home/Home.jsx";
 import {Routes, Route} from 'react-router-dom';
 import GamesCatalogue from "./components/GameCatalogue/GamesCatalogue.jsx";
 import GamesCreate from "./components/GamesCreate/GamesCreate.jsx";
+import Login from "./components/auth/Login/Login.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/games" element={<GamesCatalogue/>} />
         <Route path="/games/create" element={<GamesCreate/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </div>
   );
