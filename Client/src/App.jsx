2d2f3857d9564c910d922d1061dda52aa7bfa404
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-04-28 01:42:30 +0300

    added gamesCatalogue component plus fix on header

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,17 @@
 import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import {Routes, Route} from 'react-router-dom';
+import GamesCatalogue from "./components/GameCatalogue/GamesCatalogue.jsx";
 
 function App() {
   return (
     <div id="box">
       <Header/>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/games" element={<GamesCatalogue/>} />
+      </Routes>
     </div>
   );
 }
