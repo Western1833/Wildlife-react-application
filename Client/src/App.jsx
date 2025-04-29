5b1448b739331298dec221464be0cd8ae4edbec5
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-04-29 22:11:16 +0300

    create authContext and use it in app

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import GamesCatalogue from "./components/GameCatalogue/GamesCatalogue.jsx";
 import GamesCreate from "./components/GamesCreate/GamesCreate.jsx";
 import Login from "./components/auth/Login/Login.jsx";
 import Register from "./components/auth/Register/Register.jsx";
 import GameDetails from "./components/GameDetails/GameDetails.jsx";
+import AuthContext from "./contexts/authContext.js";
 // import { useState } from "react";
 
 function App() {
@@ -17,16 +18,18 @@ function App() {
 
   return (
     <div id="box">
-      <Header/>
+      <AuthContext.Provider>
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/games" element={<GamesCatalogue/>} />
-        <Route path="/games/create" element={<GamesCreate/>} />
-        <Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler}/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/games/:id/details" element={<GameDetails/>} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/games" element={<GamesCatalogue />} />
+          <Route path="/games/create" element={<GamesCreate />} />
+          <Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/games/:id/details" element={<GameDetails />} />
+        </Routes>
+      </AuthContext.Provider>
     </div>
   );
 }
