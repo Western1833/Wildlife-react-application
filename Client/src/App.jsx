b3cb19e489f771191e2bca8e51f81bd1ab785096
Western1833 <145915895+Western1833@users.noreply.github.com>
2025-04-29 22:56:24 +0300

    app.js works with authContext.js

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,36 +1,41 @@
 import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import GamesCatalogue from "./components/GameCatalogue/GamesCatalogue.jsx";
 import GamesCreate from "./components/GamesCreate/GamesCreate.jsx";
 import Login from "./components/auth/Login/Login.jsx";
 import Register from "./components/auth/Register/Register.jsx";
 import GameDetails from "./components/GameDetails/GameDetails.jsx";
 import AuthContext from "./contexts/authContext.js";
-// import { useState } from "react";
+import * as authService from './services/authService.js';
+import { useState } from "react";
 
 function App() {
-  // const [auth, setAuth] = useState({});
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState({});
 
-  const loginSubmitHandler = (values) => {
-    console.log(values)
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password);
+    
+    setAuth(result);
+    navigate('/');
   }
 
   return (
-    <div id="box">
-      <AuthContext.Provider>
+    <AuthContext.Provider value={{loginSubmitHandler, ...auth}}>
+      <div id="box">
         <Header />
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/games" element={<GamesCatalogue />} />
           <Route path="/games/create" element={<GamesCreate />} />
-          <Route path="/login" element={<Login loginSubmitHandler={loginSubmitHandler} />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/games/:id/details" element={<GameDetails />} />
         </Routes>
+      </div>
       </AuthContext.Provider>
-    </div>
   );
 }
 
